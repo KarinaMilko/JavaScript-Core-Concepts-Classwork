1c7b8c64a57a8e0cbef3e21e8c389920d3e6d7b9
Karina <milkokarina13@gmail.com>
2024-05-12 22:01:17 +0300

    Add templateLiteral

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// let price1 = 600;
+// let discount1 = 0.9;
+// let price2 = 800;
+// let discount2 = 0.85;
+
+// let totalPrice = discount1 * price1 + discount2 * price2;
+// console.log("Total:", totalPrice, "hrn");
+// const firstName = prompt("Input name:", "anonimus");
+// const lastName = prompt("Input surname:", "noname");
+// console.log(firstName);
+// alert(firstName + " " + lastName);
+// document.write("<h2>" + firstName + " " + lastName + "<h2/>");
+// const count = Number(prompt("Кількість товару", "1"));
+// const price = 200;
+// console.log(count * price);
+const amountOfitems = +prompt("Input amount of items:");
+const itemPrice = 200;
+
+console.log("Order price:", amountOfitems * itemPrice);
+
+2 ** (3 ** 2);
---
+++ assets/js/templateLiteral.js
@@ -0,0 +1,13 @@
+const greeting = "Hello";
+const name = "Test";
+console.log(greeting + " " + name + "!");
+const resultString = `${greeting} ${name}!`;
+console.log(resultString);
+
+// 1 + 10 = 11
+const a = 1;
+const b = 10;
+console.log(a + b);
+console.log("a+b= :>>", a + b);
+const resultNumber = `${a} + ${b} = ${a + b}`;
+console.log(resultNumber);
