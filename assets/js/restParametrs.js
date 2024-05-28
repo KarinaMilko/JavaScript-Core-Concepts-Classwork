a5f31bf91449313299d8376811ab5f07e94d16f3
Karina <milkokarina13@gmail.com>
2024-05-28 19:30:58 +0300

    Add rest parametrs

--- assets/js/restParametrs.js
+++ assets/js/restParametrs.js
@@ -46,3 +46,101 @@ accum("+", 1, 2, 8, 10); // => 21
 
 accum("*", 1, 2, 8); // => 16
 accum("*", 1, 2, 8, 10); // => 160
+
+// Arguments
+// Необмежена кількість аргументів
+
+// 1 arguments - псевдомасив (пронумеровані + length)+
+
+// arguments приймає усі перераховані при виклику функції аргументи,
+// навіть якщо при оголошенні функції вказано імена для перших кількох параметрів
+
+// Ex. a - arguments[0], b - arguments[1]
+
+// arguments - колекція для збереження аргументів функції
+
+function f() {
+  console.log(arguments);
+}
+f(1, 2);
+
+function f(a, b) {
+  console.log(arguments); // arguments включає всі параметри (1, 2,3)
+}
+f(1, 2, 3);
+
+// Написати функцію для підрахунку суми необмеженої кількості елементів
+
+function sum() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    console.log("i :>> ", i);
+    sum += arguments[i];
+  }
+  return sum;
+}
+const sumOfArgs = sum(1, 2, 3, 4, 5, 6);
+console.log(sumOfArgs);
+
+// Task: написати функцію для розрахунку добутку
+// необмеженої кількості переданих аргументів
+function dob() {
+  let dob = 1;
+  for (let i = 0; i < arguments.length; i++) {
+    dob *= arguments[i];
+  }
+  return dob;
+}
+const dobOfargs = dob(1, 2, 3, 4, 5, 6);
+console.log(dobOfargs);
+// АБО
+// Task: написати функцію виду
+// calculate(operation, operand1, operand2,..., operandN),
+// де operation - це операція (+, *), яка має бути виконана над операндами
+// operation - arguments[0], тому цикл перебираємо з 1+
+// operation = arguments[0]
+
+function calculate(operation) {
+  let accum;
+  if (operation === "+") {
+    accum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      accum += arguments[i];
+    }
+  } else if (operation === "*") {
+    accum = 1;
+    for (let i = 0; i < arguments.length; i++) {
+      accum *= arguments[i];
+    }
+  } else {
+    return undefined;
+  }
+  return accum;
+}
+
+const result = calculate("*", 1, 2, 3, 4);
+console.log(result);
+
+// переробити під rest параметри
+
+function calculate(operation, ...operands) {
+  let accum;
+  if (operation === "+") {
+    accum = 0;
+    for (let i = 0; i < operands.length; i++) {
+      accum += operands[i];
+    }
+  } else if (operation === "*") {
+    accum = 1;
+    for (let i = 0; i < operands.length; i++) {
+      accum *= operands[i];
+    }
+  } else {
+    return undefined;
+  }
+  return accum;
+}
+// arguments              0   1  2  3  4
+const result = calculate("*", 1, 2, 3, 4);
+// ...operands                0  1  2  3
+console.log(result);
